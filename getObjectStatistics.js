42fd8efc6a8a92b6156866a3ccd4fc91ebef97eb
ba <bacloud14@gmail.com>
2022-09-04 11:23:06 +0200

    Create getObjectStatistics.js

---
+++ getObjectStatistics.js
@@ -0,0 +1,70 @@
+// Helpers
+const pluck = (arr,key)=>arr.map(r=>r[key])
+const isScalar = (oo)=>['boolean', 'string', 'number', 'undefined', 'function'].indexOf(typeof (oo)) >= 0
+const isFloat = (n)=>n === +n && n !== (n | 0)
+const isInteger = (n)=>n === +n && n === (n | 0)
+const asc = arr=>arr.sort((a,b)=>a - b);
+const sum = arr=>arr.reduce((a,b)=>a + b, 0);
+const mean = arr=>sum(arr) / arr.length;
+// sample standard deviation
+const std = (arr)=>{
+    const mu = mean(arr);
+    const diffArr = arr.map(a=>(a - mu) ** 2);
+    return Math.sqrt(sum(diffArr) / (arr.length - 1));
+}
+const quantile = (arr,q)=>{
+    const sorted = asc(arr);
+    const pos = (sorted.length - 1) * q;
+    const base = Math.floor(pos);
+    const rest = pos - base;
+    if (sorted[base + 1] !== undefined) {
+        return sorted[base] + rest * (sorted[base + 1] - sorted[base]);
+    } else {
+        return sorted[base];
+    }
+}
+const q25 = arr=>quantile(arr, .25);
+const q50 = arr=>quantile(arr, .50);
+const q75 = arr=>quantile(arr, .75);
+const median = arr=>q50(arr);
+
+// Get any (with the limitation above) object statistics (like Python/Pandas DataFrame#summary method)
+function getObjectStatistics(object, key='__roo_t_', first=true) {
+    if (first && isScalar(object)) return null
+    else if (isScalar(object)) return { type: typeof (object) === 'number' ? (isFloat(object) ? 'float' : 'integer') : typeof (object) }
+    
+    if (first && Array.isArray(object)) return null
+    // if(!!(object && object.constructor && object.call && object.apply)) return null
+    let min, firstObjKey, secondObjKey
+    if (Array.isArray(object) && !isNaN(min = Math.min.apply(null, object))) {
+        const ret = {
+            type: 'array',
+            min: min,
+            max: Math.max.apply(null, object),
+            avg: object.reduce((a,b)=>a + b, 0) / object.length,
+            q25: q25(object),
+            q50: q50(object),
+            q75: q75(object),
+            median: median(object),
+            std: std(object)
+        }
+        return ret
+    } else if (Array.isArray(object) && object.length > 1 && (firstObjKey = Object.keys(object[0]))[0] === (secondObjKey = Object.keys(object[1]))[0]) {
+        let firstObjectKeys = Object.keys(object[0])
+        let secondObjectKeys = Object.keys(object[1])
+        const intersection = firstObjectKeys.filter(value=>secondObjectKeys.includes(value));
+        const ret = {}
+        for (let index = 0; index < intersection.length; index++) {
+            const key = intersection[index]
+            const arr = pluck(object, key)
+            ret[`array_${key}`] = getObjectStatistics(arr, key, false)
+        }
+        return ret
+    }
+    const finalResult = {}
+    for (const property in object) {
+        const value = object[property]
+        finalResult[`${property}`] = getObjectStatistics(value, property, false)
+    }
+    return finalResult
+}
