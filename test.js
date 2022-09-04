d2a94a20bd4a79aa4765a6965a48e1fca1e01cac
ba <bacloud14@gmail.com>
2022-09-04 16:32:01 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,29 @@
+// Helpers
+const pluck = (arr, key) => arr.map(r => r[key])
+const isScalar = (oo) => ['boolean', 'string', 'number', 'undefined', 'function'].indexOf(typeof (oo)) >= 0
+const intersect = (ob1, obj2) => ob1.filter(v => obj2.includes(v));
+const isFloat = (n) => n === +n && n !== (n | 0)
+const isInteger = (n) => n === +n && n === (n | 0)
+const asc = arr => arr.sort((a, b) => a - b);
+const sum = arr => arr.reduce((a, b) => a + b, 0);
+const mean = arr => sum(arr) / arr.length;
+// sample standard deviation
+const std = (arr) => {
+    const mu = mean(arr);
+    const diffArr = arr.map(a => (a - mu) ** 2);
+    return Math.sqrt(sum(diffArr) / (arr.length - 1));
+}
+const quantile = (arr, q) => {
+    const sorted = asc(arr);
+    const pos = (sorted.length - 1) * q;
+    const base = Math.floor(pos);
+    const rest = pos - base;
+    if (sorted[base + 1] !== undefined) {
+        return sorted[base] + rest * (sorted[base + 1] - sorted[base]);
+    } else {
+        return sorted[base];
+    }
+}
 const q25 = arr => quantile(arr, .25);
 const q50 = arr => quantile(arr, .50);
 const q75 = arr => quantile(arr, .75);
