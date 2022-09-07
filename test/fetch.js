fe532516e9e3afb9d9bb6945bbfd74e4dede7873
bacloud14 <bacloud14@gmail.com>
2022-09-07 17:19:12 +0200

    logg

--- test/fetch.js
+++ test/fetch.js
@@ -1,10 +1,16 @@
 import ObjectStatistics from "../lib/ObjectStatistics.js"
 import fetch from 'node-fetch';
 import ndjson from 'ndjson'
+// import {createBandwidthThrottleGroup} from 'bandwidth-throttle-stream';
+
 import { config as dotenv } from 'dotenv'
 dotenv()
+// const bandwidthThrottleGroup = createBandwidthThrottleGroup({
+//     bytesPerSecond: 100 // 5KB/s
+// });
+// const throttle = bandwidthThrottleGroup.createBandwidthThrottle(100);
 
-const stats = new ObjectStatistics()
+const stats = new ObjectStatistics(true)
 async function runtest(pathname) {
     console.log(`pathname=${pathname}`);
     let res = await fetch(pathname, {
@@ -14,10 +20,12 @@ async function runtest(pathname) {
         }
     });
     if (!res.ok) throw new Error(`unexpected response ${res.statusText}`);
-    res.body.pipe(ndjson.parse()).on('data', function (obj) {
-        // console.log(obj)
+    res.body./*pipe(throttle).*/pipe(ndjson.parse()).on('data', async function (obj) {
+        console.log(obj)
         let cc = stats.getStats(obj)
+        console.log('Object stats::')
         console.log(cc)
+
     })
 }
 
