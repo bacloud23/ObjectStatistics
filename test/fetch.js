479b259072078de6e2a81a392e689fac62563f5c
bacloud14 <bacloud14@gmail.com>
2022-09-07 13:28:16 +0200

    wip stream

--- test/fetch.js
+++ test/fetch.js
@@ -1,19 +1,26 @@
 import ObjectStatistics from "../lib/ObjectStatistics.js"
 import fetch from 'node-fetch';
 import ndjson from 'ndjson'
+import { config as dotenv } from 'dotenv'
+dotenv()
 
 const stats = new ObjectStatistics()
 async function runtest(pathname) {
     console.log(`pathname=${pathname}`);
-    let res = await fetch(pathname);
+    let res = await fetch(pathname, {
+        headers: {
+            Authorization: 'Bearer ' + process.env.lichessToken,
+            Accept: "application/x-ndjson"
+        }
+    });
     if (!res.ok) throw new Error(`unexpected response ${res.statusText}`);
     res.body.pipe(ndjson.parse()).on('data', function (obj) {
-        console.log(obj)
+        // console.log(obj)
         let cc = stats.getStats(obj)
         console.log(cc)
     })
 }
 
 (async function () {
-    runtest('https://dummyjson.com/products?limit=1000');
+    runtest('https://lichess.org/api/tv/feed');
 })();
