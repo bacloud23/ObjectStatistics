ef88b3876492fd4f187c284a10e8f2581b1a14d6
bacloud14 <bacloud14@gmail.com>
2022-09-08 17:15:00 +0200

    moving avg

--- test/fetch.js
+++ test/fetch.js
@@ -19,14 +19,20 @@ async function runtest(pathname) {
             Accept: "application/x-ndjson"
         }
     });
+    let co = 0
     if (!res.ok) throw new Error(`unexpected response ${res.statusText}`);
     res.body./*pipe(throttle).*/pipe(ndjson.parse()).on('data', async function (obj) {
-        console.log(obj)
+        console.log(typeof (obj))
+        obj.d ? console.log(obj.d.players) : null
         let cc = stats.getStats(obj)
-        console.log('Object stats::')
-        console.log(cc)
-
+        // console.log('Object stats::')
+        // console.log(cc)
+        if (co++ > 5)
+            console.log(stats.memory)
+    }).on('end', () => {
+        console.log(stats.memory)
     })
+
 }
 
 (async function () {
