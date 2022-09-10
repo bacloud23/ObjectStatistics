cc19eac27e918b743b23c3ff1292a56ee363f3c6
bacloud14 <bacloud14@gmail.com>
2022-09-10 18:16:41 +0200

    encode decode url

--- routes/sync.js
+++ routes/sync.js
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from 'uuid'
+import ObjectStatistics from '../lib/ObjectStatistics'
 
 async function routes(fastify, options) {
     // Channels is a map of { key: socket }
@@ -21,12 +22,12 @@ async function routes(fastify, options) {
             ws.ping()
         })
     }, 100000)
-    
+
     setInterval(() => {
         console.log(`seeing all clients`)
         for (let client of fastify.websocketServer.clients) {
             if (client.readyState === 1) console.log(client.id)
-       }
+        }
     }, 2000);
 
     // Periodically refresh sockets
@@ -66,14 +67,18 @@ async function routes(fastify, options) {
             message = JSON.parse(message.toString())
             broadcast({ sender: channel, ...message }, channel)
         })
+
+
+        ObjectStatistics(true)
     })
 
     function heartbeat() {
         this.isAlive = true
     }
 
-    function validChannel(channel) {
-        return true
+    function validChannel(url) {
+        url = decodeURIComponent(url)
+        return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(url);
     }
 
     function refreshChannels(channels) {
